51ae92b5be98ce7efe2bf5fcec20d9160fff86b8
buhleqampi <buhle3857@gmail.com>
2024-03-18 16:39:52 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+const fruits = ["mango", "banana", "oranges", "grapes", "apples"]
+const numberOfFruits = fruits.length
+const mango = fruits[0]
+const orange = fruits[2]
+const removeApple = fruits.pop()
+const removeMango = fruits.shift()
+const removeOrange = fruits.slice(1)
+const addLemon = fruits.push("lemon")
+const addKiwi = fruits.unshift("kiwi")
+
+// console.log(removeMango)
+// console.log(removeApple)
+console.log(removeOrange)
+//  console.log(addLemon)
+// console.log(addKiwi)
+console.log(fruits)
+
