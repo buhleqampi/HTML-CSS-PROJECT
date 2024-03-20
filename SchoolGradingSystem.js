d010a2287d948c56300ae056fc860cd9f8c8bf69
buhleqampi <buhle3857@gmail.com>
2024-03-20 13:26:50 +0200

    loops

--- SchoolGradingSystem.js
+++ SchoolGradingSystem.js
@@ -1,16 +1,16 @@
-const gradeCate = [
-    {category:"A",interval:"80-100" },
-    {category:"B",interval:"60-79"},
-    {category:"C",interval:"50-59"},
-    {category:"F",interval:"0-49"}
-]
+// const gradeCate = [
+//     {category:"A",interval:"80-100" },
+//     {category:"B",interval:"60-79"},
+//     {category:"C",interval:"50-59"},
+//     {category:"F",interval:"0-49"}
+// ]
 
-let student1={
-    name:"john",
-    surname:"Doe",
-    mark:34,
-    gradeCate:"unknown"
-}
+// let student1={
+//     name:"john",
+//     surname:"Doe",
+//     mark:34,
+//     gradeCate:"unknown"
+// }
 
 // let markPoint = student1.mark;
 // if (markPoint >= 80 && markPoint <= 100) {
@@ -26,6 +26,46 @@ let student1={
 // console.log("Your grade is",student1.gradeCate);
 
 // for loop
-for(i){
+// for(j=0;j<=10 ;j++) {
+// for(i=0; i<10; i++){
+//     console.log(i)
+// }
+// }
+// const Arr = [1,2,3,4,5,6,7,8,9,10]
 
+// for(n = 1; n<= Arr.length;n++){
+
+//     console.log(n)
+// }
+// const Arr2 = [3,6,9,12,15,18,21,24,27,30]
+
+// for(i=0; i < Arr2.length*3; i+3){
+//     console.log(i)
+    
+// }
+// Class Example
+// const Arr3 = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+// for (i=Arr3.length-1; i>=0; i--){
+//     console.log(i)
+// }
+// class activity
+const Arr4 = [26,1,2,3,4,5,6,7,8,9,10,11,12,40]
+
+for (i=0; i < Arr4.length;i++){
+    console.log(Arr4[i] * 2)
 }
+// The solution
+const myArr = []
+myArr.push(Arr4[i]*2)
+console.log(myArr[i])
+
+// const Arr6 = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+// for (i=0; i < Arr6.length*2+1;i+=2){
+//     console.log(i)
+// }
+// const Arr5 = [2,4,6,8,10,12,14,16,18,20,22,24]
+// for(i=2; i<Arr5.length,i++;){
+//     console.log(i)
+// }
