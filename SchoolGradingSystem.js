3e2473b2046637475c9b47a076456deb9525b842
buhleqampi <buhle3857@gmail.com>
2024-03-22 14:55:33 +0200

    loops

--- SchoolGradingSystem.js
+++ SchoolGradingSystem.js
@@ -1,97 +1,79 @@
-// 19 March -Conditional Statements & Arrays, Objects
-// const gradeCate = [
-//     {category:"A",interval:"80-100" },
-//     {category:"B",interval:"60-79"},
-//     {category:"C",interval:"50-59"},
-//     {category:"F",interval:"0-49"}
-// ]
+// // 19 March -Conditional Statements & Arrays, Objects
+const gradeCate = [
+    {category:"A",interval:"80-100" },
+    {category:"B",interval:"60-79"},
+    {category:"C",interval:"50-59"},
+    {category:"F",interval:"0-49"}
+]
 
-// let student1={
-//     name:"john",
-//     surname:"Doe",
-//     mark:34,
-//     gradeCate:"unknown"
-// }
+let student1={
+    name:"john",
+    surname:"Doe",
+    mark:34,
+    gradeCate:"unknown"
+}
 
-// let markPoint = student1.mark;
-// if (markPoint >= 80 && markPoint <= 100) {
-//     student1.gradeCate = "A";
-// } else if (markPoint >= 60 && markPoint <= 79) {
-//     student1.gradeCate = "B";
-// } else if (markPoint >= 50 && markPoint <= 59) {
-//     student1.gradeCate = "C";
-// } else if (markPoint >= 0 && markPoint <= 49) {
-//     student1.gradeCate = "F";
-// }
+let markPoint = student1.mark;
+if (markPoint >= 80 && markPoint <= 100) {
+    student1.gradeCate = "A";
+} else if (markPoint >= 60 && markPoint <= 79) {
+    student1.gradeCate = "B";
+} else if (markPoint >= 50 && markPoint <= 59) {
+    student1.gradeCate = "C";
+} else if (markPoint >= 0 && markPoint <= 49) {
+    student1.gradeCate = "F";
+}
 
 // console.log("Your grade is",student1.gradeCate);
 
 // 20 March - Loops
 // for loop
-// for(j=0;j<=10 ;j++) {
-// for(i=0; i<10; i++){
-//     console.log(i)
-// }
-// }
-// const Arr = [1,2,3,4,5,6,7,8,9,10]
+for(j=0;j<=10 ;j++) {
+for(i=0; i<10; i++){
+    // console.log(i)
+}
+}
+const Arr = [1,2,3,4,5,6,7,8,9,10]
 
-// for(n = 1; n<= Arr.length;n++){
+for(n = 1; n<= Arr.length;n++){
 
-//     console.log(n)
-// }
-// const Arr2 = [3,6,9,12,15,18,21,24,27,30]
+    // console.log(n)
+}
 
-// for(i=0; i < Arr2.length*3; i+3){
-//     console.log(i)
-    
-// }
-// Class Example
-// const Arr3 = [1,2,3,4,5,6,7,8,9,10,11,12]
-
-// for (i=Arr3.length-1; i>=0; i--){
-//     console.log(i)
-// }
-// class activity
-// const Arr4 = [26,1,2,3,4,5,6,7,8,9,10,11,12,40]
-
-// for (i=0; i < Arr4.length;i++){
-//     console.log(Arr4[i] * 2)
-// }
-// The solution
-// const myArr = []
-// myArr.push(Arr4[i]*2)
-// console.log(myArr[i])
-
-
-// const Arr6 = [1,2,3,4,5,6,7,8,9,10,11,12]
-
-// for (i=0; i < Arr6.length*2+1;i+=2){
-//     console.log(i)
-// }
-// const Arr5 = [2,4,6,8,10,12,14,16,18,20,22,24]
-// for(i=2; i<Arr5.length,i++;){
-//     console.log(i)
-// }
+// Class Challenge
+const myArr = []
+const Arr3 = [26,1,2,3,4,5,6,7,8,9,10,11,12]
+    for (i=0; i < Arr3.length;i++){
+        myArr[i] = Arr3 [i]*2;
+        // console.log(myArr)
+    }
+// Push method
+const myArr1 = []
+const Arr4 = [26,1,2,3,4,5,6,7,8,9,10,11,12]
+    for(i=0; i<Arr4.length; i++){
+        myArr1.push(Arr4[i]*2)
+}
+// console.log(myArr1)
 
 // Functions
-// function noneVoid(age){
-//     const result =age + 1;
-//     return result
-// }
+function noneVoid(age){
+    const result =age + 1;
+    return result
+}
 
-// function fvoid(age){
-//     let result= age + 1;
-// }
+function fvoid(age){
+    let result= age + 1;
+}
 // console.log(noneVoid(5))
 // console.log(fvoid(5))
 
-// function strReverse(str) { 
-//     let reversed = ""
-//     for (let i = str.length - 1; i >= 0; i--) { 
-//         reversed += str[i]; 
-//     } 
-//    return reversed
-// }  
+function strReverse(str) { 
+    let reversed = ""
+    for (let i = str.length - 1; i >= 0; i--) { 
+        reversed += str[i]; 
+    } 
+   return reversed
+}  
 
 // console.log (strReverse("Buhle"))
 
